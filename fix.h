8caf85df5a42b7b85be0641efad1488305423570
lhf <lhf@tecgraf.puc-rio.br>
2023-05-31 21:56:35 -0300

    Add files via upload

--- fix.h
+++ fix.h
@@ -1,8 +1,11 @@
+/* support Lua 5.3+ */
 #if LUA_VERSION_NUM > 502
 #define LUA_QL(x)	"'" x "'"
 #define LUA_QS		LUA_QL("%s")
 #endif
 
+/* avoid these Haru functions when libpng is not available */
+/* avoid 'unused variable' warnings */
 #ifndef LIBHPDF_HAVE_LIBPNG
 #define EMPTY 0; (void) pdf; (void) filename
 #define HPDF_LoadPngImageFromFile(pdf,filename) EMPTY
