999249fb6c2f6a786e1c95e681f42bedbb6f09dc
lhf <lhf@tecgraf.puc-rio.br>
2025-03-17 09:20:23 -0300

    Add files via upload

--- fix.h
+++ fix.h
@@ -4,10 +4,17 @@
 #define LUA_QS		LUA_QL("%s")
 #endif
 
+#if HPDF_VERSION_ID <= 20404
 /* avoid these Haru functions when libpng is not available */
 /* avoid 'unused variable' warnings */
 #ifndef LIBHPDF_HAVE_LIBPNG
 #define EMPTY 0; (void) pdf; (void) filename
 #define HPDF_LoadPngImageFromFile(pdf,filename) EMPTY
 #define HPDF_LoadPngImageFromFile2(pdf,filename) EMPTY
+#endif
+#endif
+
+/* new in libHaru 2.4.5 */
+#ifndef HPDF_FAILD_TO_ALLOC_MEM
+#define HPDF_FAILD_TO_ALLOC_MEM HPDF_FAILED_TO_ALLOC_MEM
 #endif
